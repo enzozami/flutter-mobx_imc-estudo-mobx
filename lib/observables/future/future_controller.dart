df93ea331cfe4b95b05fc2a6e49f542b6037f4bd
Enzo Zamineli <enzozamineli@gmail.com>
2025-09-10 13:58:25 -0300

    ObservableFuture [2]

--- lib/observables/future/future_controller.dart
+++ lib/observables/future/future_controller.dart
@@ -5,10 +5,16 @@ class FutureController = FutureControllerBase with _$FutureController;
 
 abstract class FutureControllerBase with Store {
   @observable
-  var nomeFuture = Future.value('').asObservable();
+  var nomeFuture = ObservableFuture.value('');
+  // var nomeFuture = Future.value('').asObservable();
 
   Future<void> buscarNome() async {
-    nomeFuture = Future.delayed(Duration(seconds: 2), () => 'Enzo Zamineli').asObservable();
+    final buscaNomeFuture =
+        Future.delayed(Duration(seconds: 2), () => 'Enzo Zamineli').asObservable();
+    nomeFuture = ObservableFuture(buscaNomeFuture);
+    // nomeFuture = buscaNomeFuture.asObservable();
+
+    // nomeFuture = Future.delayed(Duration(seconds: 2), () => 'Enzo Zamineli').asObservable();
     // return 'Enzo Zamineli';
   }
 }
